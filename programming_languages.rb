3d40a6c0a33597da228710bc1414cbcc52a7efef
Otabek Kamalov <otakam86@gmail.com>
2018-07-25 00:29:06 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,10 +3,7 @@ def reformat_languages(languages)
   new_hash = {}
  languages.each do |key1,value1|
     value1.each do |key2,value2|
-      
-      
-      
-      value2.each do |key3,value3|
+     value2.each do |key3,value3|
         if new_hash[key2] == nil 
         new_hash[key2]={key3 => value3}
         end

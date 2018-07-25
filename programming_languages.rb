d0530d87208783adae1e459b575ebf6095a37528
Otabek Kamalov <otakam86@gmail.com>
2018-07-25 00:26:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,26 +1,28 @@
 require 'pry'
 def reformat_languages(languages)
   new_hash = {}
-  compare_variable = "" 
+ 
+  
   languages.each do |key1,value1|
     value1.each do |key2,value2|
       
+      
+      
       value2.each do |key3,value3|
+        if new_hash[key2] == nil 
         new_hash[key2]={key3 => value3}
-          
-          if new_hash[key2][:style] == compare_variable && key2 == :javascript 
-          new_hash[key2][:style].push(key1)
+        end
+        
+          if new_hash[key2][:style] == nil 
+          new_hash[key2][:style] = [key1]
           else
-          new_hash[key2][:style]=[key1]
+          new_hash[key2][:style].push(key1)
+          
           end
-          
-          
        
       end
     end
-     compare_variable = key1 
+    
     end
      new_hash
   end
- 
-
